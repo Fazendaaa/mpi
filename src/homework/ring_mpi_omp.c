591bff08e6a65508d3d3f7b5e534b740a061d5f1
Fazendaaa <lucas.carotta@outlook.com>
2017-10-18 08:25:29 -0200

    Updating thread creation logic.

--- src/homework/ring_mpi_omp.c
+++ src/homework/ring_mpi_omp.c
@@ -1,13 +1,14 @@
 /*
     Inicializando-se  uma  variável  com um valor zero e a passando para outros processos que deverão abrir threads para 
     incrementar  a  variável  recebida,  assim como o próprio processo deverá incrementá-la para que, logo em sequida, a
-    envie para o próximo processo para que ocorrá o mesmo até o anel ser totalmente percorrido.
+    envie  para  o  próximo processo para que ocorrá o mesmo até o anel ser totalmente percorrido. O número de processos
+    deve ser passado pelo usuário.
 
                                                  +--------+
                                                  |        |
-                    +------+            +--------+  x = 0 +-------+               +------+
+                    +------+            +---->---+  x = 0 +--->---+               +------+
                     |      |            |        |        |       |               |      |
-                    | x+=1 +---+        |        +--------+       |          +----+ x+=1 |
+                    | x+=1 +---+        ^        +--------+       ↓          +----+ x+=1 |
                     |      |   |        |                         |          |    |      |
                     +------+   |    +---+----+                +---+----+     |    +------+
                     |      |   |    |        |                |        |     |    |      |
@@ -15,9 +16,9 @@
                     |      |   |    |        |                |        |     |    |      |
                     +------+   |    +---+----+                +---+----+     |    +------+
                     |      |   |        |                         |          |    |      |
-                    | x+=1 +---+        |        +--------+       |          +----+ x+=1 |
+                    | x+=1 +---+        ^        +--------+       ↓          +----+ x+=1 |
                     |      |            |        |        |       |               |      |
-                    +------+            +--------+  x+=1  +-------+               +------+
+                    +------+            +----<---+  x+=1  +---<---+               +------+
                                                  |        |
                                                  +---+----+
                                                      |
@@ -29,37 +30,47 @@
                                            |      |      |      |
                                            +------+------+------+
 */
-#include <stdio.h>
 #include <mpi.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char **argv) {
     /*  variáveis MPI   */
     int world_size, world_rank, token;
     /*  variáveis OMP   */
-    int this_thread, n_threads, max_threads = 3;
-
+    int this_thread, n_threads;
+    
     /*  O  número  padrão  de  processos  no  anel  é o passado com a flag '-n' na chamada do programa como argumento, o 
         profesor pediu apenas para quatro, mas caso queira mudar isso, só alterar esse valor no próprio Makefile.
     */
     MPI_Init(&argc, &argv);
-    /*  Essa chamada que pega o 'id' do processo    */
+    /*  Essa chamada pega o 'id' do processo    */
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    /*  Essa chamada pega o número de processos, '-n', passado para o programa  */
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    /*  Como  o professor limitou o número de chamadas que cada processo pode fazer com apenas três threads, essa função
-        garantirá isso.
+    /*  Como  o  professor limitou o número de chamadas que cada processo pode fazer ao número de processos, essa função
+        garantirá  que o número de threads seja o passado pelo '-n' menos um. O menos um é porque apesar de o processo 0
+        ser  criado,  ele servirá apenas para incializar os outros, que realizarão tarefas de abrir em threads, com isso
+        o número the threads será igual ao número de processos que as chamam.
     */
-    omp_set_num_threads(max_threads);
+    omp_set_num_threads(world_size-1);
 
-    /*  Caso seja o primeiro processo chamando, envia zero para os outros processos do anel.    */
-    if(0 == world_rank) {
+    /*  Nessa primeira parte se trabalhará com o número a ser passado, idependemente do processo que o está enviando. */
+
+    if (0 == world_rank) {
+        /*  Caso seja o primeiro processo chamando, token, que é o valor a ser enviado, será inicializado com zero. */
         token = 0;
     } else {
         /*  Receba o valor enviado pelo processo anterior do anel   */
         MPI_Recv(&token, 1, MPI_INT, world_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("\nProcess %d received token %d from process %d\n\n", world_rank, token, world_rank-1);
 
+        /*  Abre a chamada para que as threads desse processo possam incrementar o valor recebido pelo processo anterior
+            no anel
+        */
         #pragma omp parallel default(shared) private(this_thread, n_threads)
         {
             this_thread = omp_get_thread_num();
@@ -74,6 +85,10 @@ int main (int argc, char **argv) {
             printf("[Process %d] Thread #%d out of %d\tToken = %d\n", world_rank, this_thread, n_threads, token);
         }
 
+        /*  Após as threads terem incrementado o valor, o próprio processo incrementa ele antes de enviar para o próximo
+            processo  no  anel e que esse trecho do 'else' seja repetido no próximo processo até se chegar ao processo 0
+            de novo.
+        */
         token += 1;
     }
 
@@ -83,7 +98,7 @@ int main (int argc, char **argv) {
     /*  Caso  se  encerre a percorrida no anel e volte de novo ao primeiro processo, este apresentará o valor atualizado
         do final da execução;
     */
-    if(0 == world_rank) {
+    if (0 == world_rank) {
         MPI_Recv(&token, 1, MPI_INT, world_size-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("\nProcess %d received token %d from process %d\n\n", world_rank, token, world_size-1);
     }
